ed83938278a7a13a7cde0d8cd1be7cb39d306926
Danis Arslanov <arslanovdk@gmail.com>
2024-02-29 15:33:42 +0500

    test 5 "Римские цифры" complited

--- objects.js
+++ objects.js
@@ -255,16 +255,74 @@
 
 // Испытания
 // 5. Римские цифры
-const roman = {
+const romanNumbers = {
   1: 'I',
+  2: 'II',
+  3: 'III',
+  4: 'IV',
   5: 'V',
+  6: 'VI',
+  7: 'VII',
+  8: 'VIII',
+  9: 'IX',
   10: 'X',
+  20: 'XX',
+  30: 'XXX',
+  40: 'XL',
   50: 'L',
+  60: 'LX',
+  70: 'LXX',
+  80: 'LXXX',
+  90: 'XC',
   100: 'C',
+  200: 'CC',
+  300: 'CCC',
+  400: 'CD',
   500: 'D',
-  1000: 'M'
-}
+  600: 'DC',
+  700: 'DCC',
+  800: 'DCCC',
+  900: 'CM',
+  1000: 'M',
+  2000: 'MM',
+  3000: 'MMM',
+};
 
 function toRoman(arabic) {
-
+  let result = '';
+  let currenArabic = arabic;
+  for (let num = 1000; num >= 1; num /= 10) {
+    const currentNum = Math.floor(currenArabic / num) * num;
+    if (currenArabic >= num) {
+      result += romanNumbers[currentNum];
+    }
+    currenArabic -= currentNum;
+  }
+  return result;
 }
+
+const arabicNumbers = {};
+for (let i = 1; i <= 3000; i += 1) {
+  arabicNumbers[toRoman(i)] = i;
+}
+
+function toArabic(roman) {
+  return arabicNumbers[roman] ?? false;
+}
+
+export { toRoman, toArabic };
+
+console.log(toRoman(1));
+console.log(toRoman(27));
+console.log(toRoman(48));
+console.log(toRoman(1024));
+console.log(toRoman(778));
+
+console.log(toArabic('I'));
+console.log(toArabic('XXVII'));
+console.log(toArabic('XLVIII'));
+console.log(toArabic('MXXIV'));
+console.log(toArabic('DCCLXXVIII'));
+
+console.log(toArabic('IIII'));
+console.log(toArabic('VX'));
