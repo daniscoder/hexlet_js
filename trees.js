99edac53b1053b176cb7bbda005f7867eb31e0c6
Danis Arslanov <arslanovdk@gmail.com>
2024-04-26 18:01:28 +0500

    add test 3

--- trees.js
+++ trees.js
@@ -1,14 +1,36 @@
 // 2. Определения
-const removeFirstLevel = (tree) => {
-  return tree
-    .flat(0)
-    .filter((elem) => Array.isArray(elem))
-    .flat();
-};
+// const removeFirstLevel = (tree) => {
+//   return tree
+//     .flat(0)
+//     .filter((elem) => Array.isArray(elem))
+//     .flat();
+// };
+//
+// // Второй уровень тут: 5, 3, 4
+// const tree1 = [[5], 1, [3, 4]];
+// console.log(removeFirstLevel(tree1)); // [5, 3, 4]
+//
+// const tree2 = [1, 2, [3, 5], [[4, 3], 2]];
+// console.log(removeFirstLevel(tree2)); // [3, 5, [4, 3], 2]
 
-// Второй уровень тут: 5, 3, 4
-const tree1 = [[5], 1, [3, 4]];
-console.log(removeFirstLevel(tree1)); // [5, 3, 4]
+// 3. Виртуальная файловая система
+import { mkdir, mkfile } from '@hexlet/immutable-fs-trees';
 
-const tree2 = [1, 2, [3, 5], [[4, 3], 2]];
-console.log(removeFirstLevel(tree2)); // [3, 5, [4, 3], 2]
+const func = () =>
+  mkdir(
+    'nodejs-package',
+    [
+      mkfile('Makefile'),
+      mkfile('README.md'),
+      mkdir('dist', []),
+      mkdir('__tests__', [mkfile('half.test.js', { type: 'text/javascript' })]),
+      mkfile('babel.config.js', { type: 'text/javascript' }),
+      mkdir('node_modules', [mkdir('@babel', [mkdir('cli', [mkfile('LICENSE')])])], {
+        owner: 'root',
+        hidden: false,
+      }),
+    ],
+    { hidden: true },
+  );
+
+console.log(func());
