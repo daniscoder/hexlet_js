e0261fa2d0b9e4bb1ef1d5fda68d20d7e6bf645f
daniscoder <arslanovdk@gmail.com>
2024-05-02 22:01:47 +0500

    add test 5

--- trees.js
+++ trees.js
@@ -36,28 +36,53 @@
 // console.log(func());
 
 // 4. Манипуляции с виртуальной файловой системой
+// import _ from 'lodash';
+// import * as trees from '@hexlet/immutable-fs-trees';
+//
+// const compressImages = (tree) => trees.mkdir(
+//   trees.getName(tree),
+//   _.cloneDeep(trees.getChildren(tree)).map((children) => {
+//     const meta = trees.getMeta(children);
+//     if (trees.isFile(children) && trees.getName(children).endsWith('.jpg')) {
+//       meta.size /= 2;
+//     }
+//     return children;
+//   }),
+//   _.cloneDeep(trees.getMeta(tree)),
+// );
+//
+// const tree = trees.mkdir('my documents', [
+//   trees.mkfile('avatar.jpg', { size: 100, attributes: { hide: false, readOnly: true } }),
+//   trees.mkfile('passport.jpg', { size: 200 }),
+//   trees.mkfile('family.jpg', { size: 150 }),
+//   trees.mkfile('addresses', { size: 125 }),
+//   trees.mkdir('presentations'),
+// ]);
+//
+// const newTree = compressImages(tree);
+// console.log(JSON.stringify(newTree, null, 2));
+
+// 5. Обход дерева
+import { mkdir, mkfile, isFile, getName, getMeta, getChildren } from '@hexlet/immutable-fs-trees';
 import _ from 'lodash';
-import * as trees from '@hexlet/immutable-fs-trees';
 
-const compressImages = (tree) => trees.mkdir(
-  trees.getName(tree),
-  _.cloneDeep(trees.getChildren(tree)).map((children) => {
-    const meta = trees.getMeta(children);
-    if (trees.isFile(children) && trees.getName(children).endsWith('.jpg')) {
-      meta.size /= 2;
-    }
-    return children;
-  }),
-  _.cloneDeep(trees.getMeta(tree)),
-);
+const downcaseFileNames = (tree) => {
+  const name = getName(tree);
+  const meta = _.cloneDeep(getMeta(tree));
 
-const tree = trees.mkdir('my documents', [
-  trees.mkfile('avatar.jpg', { size: 100, attributes: { hide: false, readOnly: true } }),
-  trees.mkfile('passport.jpg', { size: 200 }),
-  trees.mkfile('family.jpg', { size: 150 }),
-  trees.mkfile('addresses', { size: 125 }),
-  trees.mkdir('presentations'),
+  if (isFile(tree)) {
+    return mkfile(name.toLowerCase(), meta);
+  }
+
+  const children = getChildren(tree);
+  const newChildren = children.map((child) => downcaseFileNames(child));
+  return mkdir(name, newChildren, meta);
+};
+
+const tree = mkdir('/', [
+  mkdir('eTc', [mkdir('NgiNx'), mkdir('CONSUL', [mkfile('config.json')])]),
+  mkfile('hOsts'),
 ]);
 
-const newTree = compressImages(tree);
-console.log(JSON.stringify(newTree, null, 2));
+console.log(tree);
+console.log(downcaseFileNames(tree));
