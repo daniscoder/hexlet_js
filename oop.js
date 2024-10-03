ddd9fe34e09ede6553398d2a6018b236fab5598f
daniscoder <arslanovdk@gmail.com>
2024-10-04 00:03:46 +0500

    add oop 4

--- oop.js
+++ oop.js
@@ -79,3 +79,14 @@
 // console.log(rat3.toString()); // '89/24'
 
 // 4. Связывание (bind)
+const bind = (obj, fn) => (...args) => fn.apply(obj, args);
+
+const obj = { number: 7 };
+const fn = function fn(number) {
+  return number + this.number;
+};
+const fnWithContext = bind(obj, fn);
+
+// Принимает столько же аргументов сколько и исходная функция
+console.log(fnWithContext(3)); // 8
+
