51a7a36c71e706bda517db47f7968367bf924b06
Danis Arslanov <arslanovdk@gmail.com>
2025-02-06 18:43:10 +0500

    oop

--- oop.js
+++ oop.js
@@ -105,57 +105,132 @@
 // // ];
 
 // 6. Конструктор
-function getX() {
-  return this.x;
+// function getX() {
+//   return this.x;
+// }
+//
+// function getY() {
+//   return this.y;
+// }
+//
+// function Point(x, y) {
+//   this.x = x;
+//   this.y = y;
+//   this.getX = getX;
+//   this.getY = getY;
+// }
+//
+// function getBeginPoint() {
+//   return this.beginPoint;
+// }
+//
+// function getEndPoint() {
+//   return this.endPoint;
+// }
+//
+// function Segment(beginPoint, endPoint) {
+//   this.beginPoint = beginPoint;
+//   this.endPoint = endPoint;
+//   this.getBeginPoint = getBeginPoint;
+//   this.getEndPoint = getEndPoint;
+// }
+//
+// function reverse(segment) {
+//   return new Segment(
+//     new Point(segment.getEndPoint().getX(), segment.getEndPoint().getY()),
+//     new Point(segment.getBeginPoint().getX(), segment.getBeginPoint().getY()),
+//   );
+// }
+//
+// const beginPoint = new Point(1, 10);
+// const endPoint = new Point(11, -3);
+//
+// const segment = new Segment(beginPoint, endPoint);
+// const reversedSegment = reverse(segment);
+//
+// // прямое обращение к свойствам приведено только в демонстрационных целях
+// console.log(segment.beginPoint.x, segment.beginPoint.y, segment.endPoint.x, segment.endPoint.y); // => 1 10 11 -3
+//
+// console.log(
+//   reversedSegment.beginPoint.x,
+//   reversedSegment.beginPoint.y,
+//   reversedSegment.endPoint.x,
+//   reversedSegment.endPoint.y,
+// ); // => 11 -3 1 10
+//
+// console.log(reversedSegment.getBeginPoint().getX());
+
+// 7. Прототипы
+// export default function Money(value, currency = 'usd') {
+//   this.value = value;
+//   this.currency = currency;
+// }
+//
+// Money.prototype.getValue = function getValue() {
+//   return this.value;
+// };
+//
+// Money.prototype.getCurrency = function getCurrency() {
+//   return this.currency;
+// };
+//
+// Money.prototype.exchangeTo = function exchangeTo(currency) {
+//   if (currency === this.currency) {
+//     return new Money(this.value, currency);
+//   }
+//   const rates = {
+//     usd: {
+//       eur: 0.7,
+//     },
+//     eur: {
+//       usd: 1.2,
+//     },
+//   };
+//   return new Money(this.value * rates[this.getCurrency()][currency], currency);
+// };
+//
+// Money.prototype.add = function add(money) {
+//   return new Money(this.value + money.exchangeTo(this.getCurrency()).getValue(), this.currency);
+// };
+//
+// Money.prototype.format = function format() {
+//   return Number(this.value).toLocaleString(undefined, {
+//     style: 'currency',
+//     currency: this.getCurrency(),
+//   });
+// };
+//
+// const money1 = new Money(100);
+//
+// // Возвращает значение
+// console.log(money1.getValue()); // 100
+// console.log(money1.getCurrency()); // 'usd'
+//
+// // Конвертирует в указанную валюту и возвращает новое значение
+// console.log(money1.exchangeTo('eur').getValue()); // 70
+//
+// const money2 = new Money(200, 'eur');
+// console.log(money2.getValue()); // 200
+// const money3 = money2.add(money1);
+// console.log(money3.getValue()); // 270
+// const money4 = money3.add(money1);
+// console.log(money4.getValue()); // 340
+//
+// console.log(money1.format()); // "$100.00"
+// console.log(money2.format()); // "€200.00"
+//
+// const money5 = new Money(10000);
+// console.log(money5.format()); // "$10,000.00"
+
+// 8. Упаковка и Распаковка (Boxing)
+export default function solution(value) {
+  const res = `Value is ${value}`; // создаем строку
+  const obj = {}; // создаем объект с прототипом Object.prototype
+  obj.value = res; // добавляем строку как свойство объекта
+  return obj;
 }
 
-function getY() {
-  return this.y;
-}
-
-function Point(x, y) {
-  this.x = x;
-  this.y = y;
-  this.getX = getX;
-  this.getY = getY;
-}
-
-function getBeginPoint() {
-  return this.beginPoint;
-}
-
-function getEndPoint() {
-  return this.endPoint;
-}
-
-function Segment(beginPoint, endPoint) {
-  this.beginPoint = beginPoint;
-  this.endPoint = endPoint;
-  this.getBeginPoint = getBeginPoint;
-  this.getEndPoint = getEndPoint;
-}
-
-function reverse(segment) {
-  return new Segment(
-    new Point(segment.getEndPoint().getX(), segment.getEndPoint().getY()),
-    new Point(segment.getBeginPoint().getX(), segment.getBeginPoint().getY()),
-  );
-}
-
-const beginPoint = new Point(1, 10);
-const endPoint = new Point(11, -3);
-
-const segment = new Segment(beginPoint, endPoint);
-const reversedSegment = reverse(segment);
-
-// прямое обращение к свойствам приведено только в демонстрационных целях
-console.log(segment.beginPoint.x, segment.beginPoint.y, segment.endPoint.x, segment.endPoint.y); // => 1 10 11 -3
-
-console.log(
-  reversedSegment.beginPoint.x,
-  reversedSegment.beginPoint.y,
-  reversedSegment.endPoint.x,
-  reversedSegment.endPoint.y,
-); // => 11 -3 1 10
-
-console.log(reversedSegment.getBeginPoint().getX());
+console.log(solution('some value').__proto__)
+console.log(solution(1) + ''); // 'Value is 1'
+console.log(solution(10) + ''); // 'Value is 10'
+console.log(solution('some value') + ''); // 'Value is some value'
