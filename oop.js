21193c4445c5361a50bebbe3579557727e7dc3cc
Danis Arslanov <arslanovdk@gmail.com>
2025-02-08 11:21:41 +0500

    oop complited

--- oop.js
+++ oop.js
@@ -306,28 +306,50 @@
 // console.log(cart.getItems());
 
 // 11. Статические свойства и методы
-export default class Time {
-  // BEGIN (write your solution here)
+// export default class Time {
+//   // BEGIN (write your solution here)
+//
+//   static fromString(time) {
+//     return new Time(...time.split(':', 2));
+//   }
+//
+//   // END
+//
+//   constructor(hours, minutes) {
+//     this.minutes = minutes;
+//     this.hours = hours;
+//   }
+//
+//   toString() {
+//     return `${this.hours}:${this.minutes}`;
+//   }
+// }
+//
+// const time1 = new Time(10, 15);
+// console.log(`The time is ${time1}`); // => 'The time is 10:15'
+//
+// const time2 = Time.fromString('10:23');
+// // автоматически вызывается метод toString()
+// console.log(`The time is ${time2}`); // 'The time is 10:23'
 
-  static fromString(time) {
-    return new Time(...time.split(':', 2));
-  }
-
-  // END
-
-  constructor(hours, minutes) {
-    this.minutes = minutes;
-    this.hours = hours;
-  }
-
-  toString() {
-    return `${this.hours}:${this.minutes}`;
+// 12. Исключения
+class ParseError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'ParseError';
   }
 }
 
-const time1 = new Time(10, 15);
-console.log(`The time is ${time1}`); // => 'The time is 10:15'
+export function parseJson(json) {
+  try {
+    return JSON.parse(json);
+  } catch (error) {
+    throw new ParseError(error);
+  }
+}
 
-const time2 = Time.fromString('10:23');
-// автоматически вызывается метод toString()
-console.log(`The time is ${time2}`); // 'The time is 10:23'
+const json = '{ "key": "value" }';
+parseJson(json); // { key: 'value' }
+
+const incorrectJson = '{ key": "value" }';
+parseJson(incorrectJson); // => ParseError: Invalid JSON string
