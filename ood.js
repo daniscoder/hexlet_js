79a9d630f3f954096c76c32e53e376ffa82c2111
Danis Arslanov <arslanovdk@gmail.com>
2025-02-10 17:54:33 +0500

    ood 5

--- ood.js
+++ ood.js
@@ -1,26 +1,130 @@
 // 3. Конфигурация
-const hasNumber = (string) => string.search(/\d/) !== -1;
+// const hasNumber = (string) => string.search(/\d/) !== -1;
+//
+// export default class PasswordValidator {
+//   options = {}
+//
+//   constructor(obj = {}) {
+//     this.options.minLength = Object.hasOwn(obj, 'minLength') ? obj.minLength : 8;
+//     this.options.containNumbers = Object.hasOwn(obj, 'containNumbers') ? obj.containNumbers : true;
+//   }
+//
+//   validate(passwd) {
+//     const res = {};
+//     if (passwd.length < this.options.minLength) {
+//       res.minLength = 'too small';
+//     }
+//     if (this.options.containNumbers && !hasNumber(passwd)) {
+//       res.containNumbers = 'should contain at least one number';
+//     }
+//     return res;
+//   }
+// }
+//
+// const validator = new PasswordValidator({ containNumbers: false });
+// console.log(validator.validate('qwertya3sdf')); // {}
+// console.log(validator.validate('qwerty')); // { minLength: 'too small' }
 
-export default class PasswordValidator {
-  options = {}
+// 4. Изменяемая конфигурация
+// export default class Truncater {
+//   static defaultOptions = {
+//     separator: '...',
+//     length: 200,
+//   };
+//
+//   static getNewOptions(oldOptions, newOptions) {
+//     return Object.assign(structuredClone(oldOptions), newOptions);
+//   }
+//
+//   constructor(options) {
+//     this.options = this.constructor.getNewOptions(this.constructor.defaultOptions, options);
+//   }
+//
+//   truncate(text, newOptions = {}) {
+//     const options = this.constructor.getNewOptions(this.options, newOptions);
+//     if (text.length > options.length) {
+//       return text.substring(0, options.length).concat(options.separator);
+//     }
+//     return text;
+//   }
+// }
+//
+// // const truncater = new Truncater();
+// // console.log(truncater.truncate('one two')); // 'one two'
+// // console.log(truncater.truncate('one two', { length: 6 })); // 'one tw...'
+//
+// const truncater = new Truncater({ length: 6 });
+// console.log(truncater.truncate('one two', { separator: '.' })); // 'one tw.'
+// console.log(truncater.truncate('one two', { length: 3 })); // 'one...'
 
-  constructor(obj = {}) {
-    this.options.minLength = Object.hasOwn(obj, 'minLength') ? obj.minLength : 8;
-    this.options.containNumbers = Object.hasOwn(obj, 'containNumbers') ? obj.containNumbers : true;
-  }
+// 5. Объекты-Сущности, Объекты-Значения и внедренные объекты
+// export default class Url {
+//   constructor(url) {
+//     this.url = new URL(url);
+//   }
+//
+//   getScheme() {
+//     return this.url.protocol.substring(0, this.url.protocol.length - 1);
+//   }
+//
+//   getHostName() {
+//     return this.url.hostname;
+//   }
+//
+//   getQueryParams() {
+//     return Object.fromEntries(this.url.searchParams);
+//   }
+//
+//   getQueryParam(key, defaultValue = null) {
+//     return Object.fromEntries(this.url.searchParams)[key] ?? defaultValue;
+//   }
+//
+//   equals(url) {
+//     return this.url.toString() === url.url.toString();
+//   }
+// }
+//
+// // const url = new Url('http://yandex.ru:80?key=value&key2=value2');
+// // console.log(url.getScheme()); // 'http'
+// // console.log(url.getHostName()); // 'yandex.ru'
+// // console.log(url.getQueryParams());
+// // // {
+// // //   key: 'value',
+// // //   key2: 'value2',
+// // // };
+// // console.log(url.getQueryParam('key')); // 'value'
+// // // второй параметр - значение по умолчанию
+// // console.log(url.getQueryParam('key2', 'lala')); // 'value2'
+// // console.log(url.getQueryParam('new', 'ehu')); // 'ehu'
+// // console.log(url.getQueryParam('new')); // null
+// // console.log(url.equals(new Url('http://yandex.ru:80?key=value&key2=value2'))); // true
+// // console.log(url.equals(new Url('http://yandex.ru:80?key=value'))); // false
+//
+// const yandexUrl = 'http://yandex.ru?key=value&key2=value2';
+// const googleUrl = 'https://google.com:80?a=b&c=d&lala=value';
+// const url = new Url(googleUrl);
+// console.log(url.equals(new Url(googleUrl.replace('80', '443'))))
 
-  validate(passwd) {
-    const res = {};
-    if (passwd.length < this.options.minLength) {
-      res.minLength = 'too small';
-    }
-    if (this.options.containNumbers && !hasNumber(passwd)) {
-      res.containNumbers = 'should contain at least one number';
-    }
-    return res;
-  }
+// 6. Fluent Interface
+export default function normalize(arr) {
+  return arr.reduce((acc, elem) => {
+    acc[elem.country.trim().toLowerCase()] = elem.name.trim().toLowerCase()
+  }, {})
 }
 
-const validator = new PasswordValidator({ containNumbers: false });
-console.log(validator.validate('qwertya3sdf')); // {}
-console.log(validator.validate('qwerty')); // { minLength: 'too small' }
+const countries = [
+  { name: 'Miami', country: 'usa' },
+  { name: 'samarA', country: '  ruSsiA' },
+  { name: 'Moscow ', country: ' Russia' },
+];
+
+console.log(normalize(countries));
+// {
+//   russia: [
+//     'moscow',
+//     'samara',
+//   ],
+//   usa: [
+//     'miami',
+//   ],
+// }
