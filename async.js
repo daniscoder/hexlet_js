f5a8a487d344db65cdd35b5620dea1e8cebdfebd
Danis Arslanov <arslanovdk@gmail.com>
2025-02-18 23:45:09 +0500

    async 6 compl

--- async.js
+++ async.js
@@ -7,15 +7,39 @@
 //   });
 // };
 
-// 5. Упорядочивание асинхронных операций
+// // 5. Упорядочивание асинхронных операций
+// import fs from 'fs';
+//
+// export const compareFileSizes = (filepath1, filepath2, cb) => {
+//   fs.stat(filepath1, (error1, { size: size1 }) => {
+//     fs.stat(filepath2, (error2, { size: size2 }) => {
+//       cb(null, Math.sign(size1 - size2));
+//     });
+//   });
+// };
+//
+// compareFileSizes('oop.js', 'ood.js', (_err, result) => console.log(result));
+
+// 6. Обработка ошибок
 import fs from 'fs';
 
-export const compareFileSizes = (filepath1, filepath2, cb) => {
-  fs.stat(filepath1, (error1, { size: size1 }) => {
-    fs.stat(filepath2, (error2, { size: size2 }) => {
-      cb(null, Math.sign(size1 - size2));
+export const move = (filepath1, filepath2, cb) => {
+  fs.readFile(filepath1, 'utf8', (err, data) => {
+    if (err) return cb(err);
+    fs.writeFile(filepath2, data, 'utf8', (err, data) => {
+      if (err) return cb(err);
+      fs.unlink(filepath1, (err) => {
+        if (err) return cb(err);
+        cb(null);
+      });
     });
   });
 };
 
-compareFileSizes('oop.js', 'ood.js', (_err, result) => console.log(result));
+move('test.txt', 'test2.txt', (error) => {
+  if (error) {
+    console.log('oops');
+    return;
+  }
+  console.log('yes!');
+});
